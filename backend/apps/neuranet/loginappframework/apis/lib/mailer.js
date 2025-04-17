37173b6d8431e8bec92d351f06080fb2e563e881
Bhaveshdhapola <bhavesh.dhapola@deeplogictech.com>
2025-04-17 15:03:57 +0530

    Add in MailGun API for sending emails

--- backend/apps/neuranet/loginappframework/apis/lib/mailer.js
+++ backend/apps/neuranet/loginappframework/apis/lib/mailer.js
@@ -9,7 +9,7 @@ const conf = require(`${APP_CONSTANTS.CONF_DIR}/smtp.json`);
 const serverMailer = require(CONSTANTS.LIBDIR+"/mailer.js");
 
 module.exports.email = async function(to, title, email_html, email_text) {
-    const auth = {user: conf.user, pass: crypt.decrypt(conf.password)};
-    const smtpConfig = {server: conf.server, port: conf.port, secure: conf.secure, from: conf.from};
+    const auth = conf.useMailGunApi ? {auth: { api_key: crypt.decrypt(conf.apikey), domain: conf.domain }} : {user: conf.user, pass: crypt.decrypt(conf.password)};
+    const smtpConfig = {server: conf.server, port: conf.port, secure: conf.secure, from: conf.from, useMailGunApi: conf.useMailGunApi};
     return (await serverMailer.email(to, title, email_html, email_text, smtpConfig, auth));
 }
